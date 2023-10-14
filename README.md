bf0c6a848ec0853e0c3e959ebf782310228948ef
Serena <Sere.Brow@Outlook.com>
2023-10-13 21:35:21 -0400

    173 passing (5s)

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # mmc6278-7-7-brown-serena
+
 Be Authentic
+
+### Final Result - Passing
+
+![Final](./passing.png "Passing")
---
+++ middleware/auth.js
@@ -0,0 +1,12 @@
+function checkAuth(req, res, next) {
+  if (req.session && req.session.loggedIn) {
+    next();
+  } else {
+    res.redirect("/login");
+  }
+  // Check req.session.loggedIn to see if the user is logged in
+  // If the user is logged in, call next()
+  // If the user is not logged in, redirect to /login
+}
+
+module.exports = checkAuth;
